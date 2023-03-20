f5f06848220e17e892a49ea735c4fc09bcad320e
MaritsIkmalY <maritsikmaly@gmail.com>
2023-03-20 10:35:47 +0700

    map list to widget

---
+++ lib/answer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+          style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(Colors.red)),
+          onPressed: selectHandler,
+          child: Text(answerText)),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -6,40 +8,65 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questionIndex = 0;
-
-    void answerQuestion() {
-      questionIndex = questionIndex + 1;
-      print('Hello World !');
-    }
-
-    var questions = [
-      'What\'s Your Favorite Color?',
-      'What\'s Your Favorite Movie?'
+    const _questions = [
+      //This is Map
+      {
+        'questionText': 'What\'s Your Favorite Color?',
+        'answer': ['Black', 'Blue', 'Yellow', 'Green']
+      },
+      {
+        'questionText': 'What\'s Your Favorite Movie?',
+        'answer': ['Kung Fu Panda', 'Pororo', 'One Piece', 'Blue Lock']
+      },
+      {
+        'questionText': 'What\'s Your Favorite Song?',
+        'answer': ['SciFi', 'Balling', 'Call Me Back', 'Isohel']
+      },
+      {
+        'questionText': 'What\'s Your Favorite Drink?',
+        'answer': ['Water', 'Coca Cola', 'Pop Ice', 'Milk']
+      },
+      {
+        'questionText': 'What\'s Your Favorite Food?',
+        'answer': ['Mie Ayam', 'Bakso', 'Ayam Goreng', 'Telur']
+      },
     ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My First App'),
+          title: const Text(
+            'My First Aps',
+          ),
         ),
         body: Column(
           // ignore: prefer_const_literals_to_create_immutables
           children: [
-            Text(
-              questions[questionIndex],
+            Question(
+              _questions[_questionIndex]['questionText'] as String,
             ),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
-            ElevatedButton(
-                onPressed: () {
-                  print('Good Job');
-                },
-                child: Text('Answer 2')),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3')),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 4')),
+            ...(_questions[_questionIndex]['answer'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
